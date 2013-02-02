109dfbc8faafbce8ed835764c31b207a4b31e16e
Phalgun Guduthur <me@phalgun.in>
2013-02-03 01:33:10 +0530

    .logs command will generate a pastebin link with the channel logs
    The .logs command will also take an argument which is used to specify the
    number of log lines to be displayed.
    
    For example, .logs 10 will give the last 10 logged lines in the channel.

--- modules/interjection.py
+++ modules/interjection.py
@@ -1,10 +1,10 @@
 from time import gmtime, strftime
 import logging
 
-logging.basicConfig(filename='/home/phalgun/coding/example.log', format='%(asctime)s:%(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', level=logging.INFO)
+logging.basicConfig(filename='/home/phalgun/coding/example.log', format='[%(asctime)s]%(message)s', datefmt='%d/%m/%Y %I:%M:%S', level=logging.INFO)
 
 def interjection(phenny, input):
-        logging.info(' '+input.nick+': '+input)
+        logging.info(' <'+input.nick+'> '+input)
 
 interjection.rule = r'.*'
 interjection.priority = 'high'
--- modules/logs.py
+++ modules/logs.py
@@ -1,8 +1,28 @@
-def logs(phenny, input):
-	f = open('/home/phalgun/coding/example.log', 'r')
-	for line in f:
-		phenny.say(line)
+import urllib
+import urllib2
 
-logs.rule = r'$nickname: logs!'
+url = 'http://pastebin.com/api/api_post.php'
+
+def logs(phenny, input):
+	text = ''
+	if input == '.logs':
+		num = 0
+	else:
+		num = int(input.split(' ')[1])
+	f = open('/home/phalgun/coding/example.log', 'r')
+	for line in f.readlines()[-num:]:
+		text = text + line
+
+	values = {'api_option':'paste',
+          'api_dev_key':'d8a90aa1dc591a75d5beeb91fd6f7e30',
+          'api_paste_code':text,
+	  'api_paste_format':'mirc'}
+
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req)
+	the_page = response.read()
+	phenny.say(input.nick+': '+the_page)
+	
+logs.commands = ['logs']
 logs.priority = 'medium'
-logs.thread = False
