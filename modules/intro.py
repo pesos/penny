20d3d611d76a98cbbee32b7f9ada390f79cd7c6c
Phalgun Guduthur <me@phalgun.in>
2013-04-13 22:23:58 +0530

    Added intro module

---
+++ modules/intro.py
@@ -0,0 +1,14 @@
+import re
+
+def intro(phenny,input):
+	if input == ".intro":
+		phenny.say("Everyone! Please introduce yourself in one line (Name, Department, Year")
+		return
+
+	else:
+		matchObj = re.match( r'.intro (.*)', input, re.M|re.I)
+		if matchObj:
+			phenny.say(matchObj.group(1)+ ": Please introduce yourself in one line (Name, Department, Year)")
+		else:
+			phenny.reply("Sorry, retry!")
+	
