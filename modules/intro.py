80f3482685d35c7b8581a17a6f0c7f74f82a4b6d
Phalgun Guduthur <me@phalgun.in>
2013-04-13 22:39:37 +0530

    tab changes

--- modules/intro.py
+++ modules/intro.py
@@ -1,14 +1,18 @@
 import re
 
 def intro(phenny,input):
-	if input == ".intro":
-		phenny.say("Everyone! Please introduce yourself in one line (Name, Department, Year")
-		return
+    if input == ".intro":
+        phenny.say("Everyone! Please introduce yourself in one line (Name, Department, Year")
+        return
 
-	else:
-		matchObj = re.match( r'.intro (.*)', input, re.M|re.I)
-		if matchObj:
-			phenny.say(matchObj.group(1)+ ": Please introduce yourself in one line (Name, Department, Year)")
-		else:
-			phenny.reply("Sorry, retry!")
-	
+    else:
+        matchObj = re.match( r'.intro (.*)', input, re.M|re.I)
+        if matchObj:
+            phenny.say(matchObj.group(1)+ ": Please introduce yourself in one line (Name, Department, Year)")
+            return
+        else:
+            phenny.reply("Sorry, retry!")
+            return
+
+intro.commands = ['intro']
+intro.priority = 'medium'	
